ee4f760e400f61acfae04d2ee2e59d6433602449
Julian Tescher <virulent@gmail.com>
2013-11-16 11:44:54 -0800

    Initial app scaffold

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.1'
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.0'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+# Use CoffeeScript for .js.coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+
+group :development, :test do
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
+end
+
+group :production do
+  # Use Postgresql as the database for Active Record
+  gem 'pg', '~> 0.17.0'
+  gem 'rails_12factor', '~> 0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joyfoodly::Application.config.secret_key_base = '46c7d5a3a6fa9d96df6abeb45dbca00e32f2cdb9c910eb3aa157e3edcc7b4ad910cf9585f422d8f5ef892eacecb9c5742182273297dfc9f95b9e8c9c1b34cda2'
