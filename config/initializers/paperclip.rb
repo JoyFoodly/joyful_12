500eed926160f2cd413490bf296b5624b52000f6
Julian Tescher <virulent@gmail.com>
2014-01-29 22:58:09 -0800

    Revert to default paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,6 @@
 Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
-Paperclip::Attachment.default_options[:url] = '/:imageable_class/:class/:style/:hash.:extension'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:imageable_class/:class/:style/:hash.:extension'
 Paperclip::Attachment.default_options[:hash_secret] = ENV['PAPERCLIP_HASH_SECRET']
 
 Paperclip.interpolates :imageable_class do |attachment, _|
