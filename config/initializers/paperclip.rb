6541715c4642fe29307cdebd297232ae63d15bb3
Julian Tescher <virulent@gmail.com>
2014-01-29 23:02:18 -0800

    Remove double s3 bucket name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,3 @@
-Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:imageable_class/:class/:style/:hash.:extension'
 Paperclip::Attachment.default_options[:hash_secret] = ENV['PAPERCLIP_HASH_SECRET']
