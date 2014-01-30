4606c91ce131dece8775943337b1e46b2c41ea2c
Julian Tescher <virulent@gmail.com>
2014-01-29 22:54:27 -0800

    Add s3 host name var

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
+Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
 Paperclip::Attachment.default_options[:url] = '/:imageable_class/:class/:style/:hash.:extension'
 Paperclip::Attachment.default_options[:hash_secret] = ENV['PAPERCLIP_HASH_SECRET']
 
