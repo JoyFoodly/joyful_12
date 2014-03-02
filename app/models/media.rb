7058e5e2c49965ea12d4ee05179133c0778876cf
Julian Tescher <virulent@gmail.com>
2014-03-01 17:20:47 -0800

    Update media thumbnail dimensions

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,9 +1,11 @@
 class Media < ActiveRecord::Base
   has_paper_trail
-  has_attached_file :file, styles: { thumbnail: '469x' }
+  has_attached_file :file, styles: { thumbnail: '' }, convert_options: {
+    thumbnail: '-gravity north -thumbnail 469x312^ -extent 469x312'
+  }
 
   validates :file, attachment_presence: true, attachment_content_type: {
-      content_type: %w[image/jpg image/jpeg image/gif image/png]
+    content_type: %w[image/jpg image/jpeg image/gif image/png]
   }
 
 end
