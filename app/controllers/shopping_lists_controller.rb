8a545cb563adaccff457a7800c127a3c7744bc1c
Julian Tescher <virulent@gmail.com>
2014-03-02 20:24:27 -0800

    Add saved shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -17,7 +17,7 @@ class ShoppingListsController < ApplicationController
   def edit
     @user = current_user
     if params[:id] == 'current'
-      @shopping_list = current_user.shopping_lists.includes(ingredient_list_items: [:ingredient]).where(completed_at: nil).first
+      @shopping_list = ShoppingList::ListBuilder.call(@user) if @shopping_list.nil?
     else
       @shopping_list = current_user.shopping_lists.includes(ingredient_list_items: [:ingredient]).find(params[:id])
     end
@@ -25,9 +25,14 @@ class ShoppingListsController < ApplicationController
 
   def update
     @shopping_list = ShoppingList.find(params[:id])
+    @shopping_list.completed_at = Time.current if params[:archive].present?
 
     if @shopping_list.update(shopping_list_params)
-      redirect_to edit_shopping_list_path(@shopping_list), notice: 'Shopping list updated!'
+      if params[:archive].present?
+        redirect_to edit_user_path(@shopping_list), notice: 'Shopping list saved!'
+      else
+        redirect_to edit_shopping_list_path(@shopping_list), notice: 'Shopping list updated!'
+      end
     else
       render :edit
     end
--- app/models/shopping_list/list_builder.rb
+++ app/models/shopping_list/list_builder.rb
@@ -6,14 +6,20 @@ class ShoppingList::ListBuilder
     @recipe = recipe
   end
 
-  def self.call(user, recipe)
+  def self.call(user, recipe = nil)
     new(user, recipe).build_list
   end
 
   def build_list
-    list = user.shopping_lists.find_or_create_by!(completed_at: nil)
+    list = user.shopping_lists.includes(ingredient_list_items: [:ingredient])
+                              .create_with(name: default_name)
+                              .find_or_create_by!(completed_at: nil)
     list.recipes << recipe if recipe.present? && !list.recipes.include?(recipe)
     list
+  end
+
+  def default_name
+    "#{Date.current.strftime('%D')} Shopping List"
   end
 
 end
