bfd1b6e55b44f755cc512240b8691199ab9b7e17
Julian Tescher <virulent@gmail.com>
2014-04-28 21:58:03 -0700

    Allow comments crawler to pull page details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! unless :comments_crawler
 
   def show
     @page = Page.find_by!(slug: params[:id])
   end
+  
+private
+
+ def comments_crawler
+   request.remote_ip =~ Regexp.new(ENV['COMMENTS_CRAWLER_IP_ADDRESS'])
+ end
 
 end
