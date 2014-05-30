687891febeec530a7e4d51571c409bc3ea3ecf3c
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2014-05-29 22:45:44 -0700

    Add qubico assets to precompile list

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Joyfoodly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w[jquery-2.1.0.js]
+    config.assets.precompile += %w[jquery-2.1.0.js qubico.css qubico.js]
   end
 end
