00009808494c8bb5d3c5717cee24fda4b1eb2062
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2014-06-01 18:21:37 -0700

    fixed get started box

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :ensure_onboarding
 
   def show
     @page = Page.find_by!(slug: params[:id])
+
+    redirect_to edit_user_path(current_user) and return if !current_user.onboarded? && @page.slug != 'ToS'
   end
 
 end
