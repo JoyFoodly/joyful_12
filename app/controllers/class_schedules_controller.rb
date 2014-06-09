5c56551e847d2d149b298e5d0b4c7c86e9493bdc
Julian Tescher <virulent@gmail.com>
2014-06-08 18:18:25 -0700

    Fix class schedule bug

--- app/controllers/class_schedules_controller.rb
+++ app/controllers/class_schedules_controller.rb
@@ -1,13 +1,18 @@
 class ClassSchedulesController < ApplicationController
   before_filter :authenticate_user!
   before_filter :ensure_onboarding
+  before_filter :set_season
   before_filter :authorize_user
 
   def show
-    @season = Season.current_season(session[:season_name])
+
   end
 
 private
+
+  def set_season
+    @season = Season.current_season(session[:season_name])
+  end
 
   def authorize_user
     unless current_user.seasons.include?(@season)
