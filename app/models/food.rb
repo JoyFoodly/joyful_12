40164bb7485f32d809a4da4d5b14cd6c983f80a7
Sameer Siruguri <siruguri@gmail.com>
2014-12-04 10:10:04 -0800

    food is now imageable

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,7 @@
 class Food < ActiveRecord::Base
   has_many :recipes, dependent: :destroy
+  has_many :images, -> { order('sort_order ASC') }, as: :imageable, dependent: :destroy
+
   has_and_belongs_to_many :video_links
   belongs_to :season
 
