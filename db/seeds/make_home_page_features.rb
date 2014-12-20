59d8706e837d94ecb56fc88ad3bd6e8a3183e2a2
Sameer Siruguri <siruguri@gmail.com>
2014-12-19 16:42:06 -0800

    more novato specific language in seeds

--- db/seeds/make_home_page_features.rb
+++ db/seeds/make_home_page_features.rb
@@ -1,11 +1,11 @@
-keys = [:box11_text, :box12_text, :box21_text, :box22_text, :box11_header, :box12_header, :box21_header, :box22_header, [:general_welcome_message, nil], [:general_welcome_message, 'NovatoCoupon'], :welcome_bullets, :section_title_one, :section_title_two, :rightcolumn_subtext, :section_subtext_one, :section_subtext_two]
+keys = [:box11_text, :box12_text, :box21_text, :box22_text, :box11_header, :box12_header, :box21_header, :box22_header, [:general_welcome_message, nil], [:general_welcome_message, 'NovatoCoupon'], [:welcome_bullets, nil], [:welcome_bullets, 'NovatoCoupon'], :section_title_one, :section_title_two, :rightcolumn_subtext, :section_subtext_one, :section_subtext_two]
 
 contents = {box11_header: 'Eat the Rainbox', box11_text: 'Feel great about the meals you make for your family. 12 veggies and fruits', box12_header: 'Time Saving Shopping List', box12_text: 'Make shopping easy with a shopping list. You can email',
             [:general_welcome_message, nil] => 'COOK VEGGIES YOUR KIDS WILL LOVE.',
             [:general_welcome_message, 'NovatoCoupon'] => 'Join me Healthy Kitchens, Healthy Lives Participants',
             welcome_bullets_old: '<li>Master 48 fruits and veggies</li><li>144 family-friendly recipes for all the year</li><li>Master 48 fruits and veggies</li><li>144 family-friendly recipes for all the year</li>',
-            welcome_bullets: '<p>Kids demand smashed cauliflower when their parents cook the Joyful12! <p> Improve the health of your children with our joyful method to cook a wide variety of fruits and vegetables every season.<P>Every purchase supports our Joyful 12 School Projects.',
-            rightcolumn_subtext: 'Get cooking lessons year round in your own home for ',
+            [:welcome_bullets, nil] => '<p>Kids demand smashed cauliflower when their parents cook the Joyful12! <p> Improve the health of your children with our joyful method to cook a wide variety of fruits and vegetables every season.<P>Every purchase supports our Joyful 12 School Projects.',
+            [:welcome_bullets, 'NovatoCoupon'] => '<p>Start cooking veggies your kids will demand.</P><p>My Joyful 12 Online Learning Lab will help you improve the health of your children with over 100 family friendly recipes to love cooking and eating seasonal veggies every month of the year.</p>',
             section_title_one: 'HOLY SWEET POTATO! COOKING DELICIOUS VEGGIES HAS NEVER BEEN EASIER',
             section_title_two: 'Give the gift of the Joyful 12!',
             section_subtext_one: 'The Joyful12 is an online kitchen learning lab that helps parents learn how to joyfully get fruits and veggies into their families\' diets.',
