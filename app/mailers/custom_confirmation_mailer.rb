bebab519a6fad51608fad2c3de55ec35d07d68da
Sunny Juneja <me@sunnyjuneja.com>
2015-03-06 15:21:28 -0800

    Only use custom coupon template if it exists.

--- app/mailers/custom_confirmation_mailer.rb
+++ app/mailers/custom_confirmation_mailer.rb
@@ -5,7 +5,7 @@ class CustomConfirmationMailer < Devise::Mailer
   default template_path: 'devise/mailer' # to make sure that you mailer uses the devise views
 
   def confirmation_instructions(record, token, opts={})
-    if record.respond_to?(:coupon_id) && !record.coupon_id.nil?
+    if record.respond_to?(:coupon_id) && !record.coupon_id.nil? && Dir.exists?("#{Rails.root}/app/views/devise/mailer/#{record.coupon_id}")
       opts[:template_path]="devise/mailer/#{record.coupon_id}"
     end
     super
