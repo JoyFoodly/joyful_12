0873fd13bf3dc1b70a519c5a8305e51844aedb77
Sunny Juneja <me@sunnyjuneja.com>
2015-06-02 15:43:46 -0700

    Fix up gifting page.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,6 +14,7 @@ class PaymentsController < ApplicationController
 
   def create_gift
     @gift = Gift.new(gift_params)
+    @gift.giver_id = current_user
     @gift.build_coupon
     @payment = Payment.new(payment_params)
     @payment.email = @gift.your_email
@@ -28,7 +29,7 @@ class PaymentsController < ApplicationController
       redirect_to root_path
     else
       errors = @payment.errors[:stripe] + @gift.errors.full_messages
-      flash[:alert] = errors.to_sentence
+      flash.now[:alert] = errors.to_sentence
       render 'gift', layout: 'sign_up_flow'
     end
   end
@@ -39,7 +40,7 @@ class PaymentsController < ApplicationController
       current_user.update(signed_up: true)
       redirect_to after_sign_in_path_for(current_user)
     else
-      flash[:alert] = @payment.errors[:stripe].to_sentence
+      flash.now[:alert] = @payment.errors[:stripe].to_sentence
       render 'new', layout: 'sign_up_flow'
     end
   end
@@ -49,7 +50,6 @@ class PaymentsController < ApplicationController
   def gift_params
     p = params.require(:gift).permit(:your_name, :your_email, :message, :their_email)
     p[:price] = @gift_price
-    p[:giver] = current_user
     p
   end
 
