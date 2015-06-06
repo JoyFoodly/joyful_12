636ea994f100c7bbfda9bef525f5b732dd073e33
Sunny Juneja <me@sunnyjuneja.com>
2015-06-06 12:54:40 -0700

    Clean up code a bit.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,10 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
   
   def create
     @user = User.new(user_params)
+    coupon = found_coupon?
+    @user.coupon_id = coupon.try(:id)
     @user.signed_up = true if @price == 0
 
     if @user.save
-      coupon.destroy if coupon = found_coupon? and coupon.gift?
+      coupon.destroy if coupon.try(:gift?)
       flash[:notice] = 'Your account was created successfully.'
       sign_in(User, @user)
       respond_with @user, location: after_sign_up_path_for(@user)
